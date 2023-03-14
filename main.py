c1cf1149d4aa08e20df925bcd4ee4ef6b78725e2
rimu254 <niccimurimi@gmail.com>
2023-03-14 06:24:20 +0200

    Theme and logic change

--- main.py
+++ main.py
@@ -24,15 +24,17 @@ def signup():
         db.insert_user(username, password, name)
         st.success('User created successfully')
 
+def login_callback():
+    st.write('st.session_state.loggedin')
 def login():
-    st.sidebar.subheader('Login')
-    username = st.sidebar.text_input('Username')
-    password = st.sidebar.text_input('Password', type='password')
+    st.subheader('Confirm Credentials')
+    username = st.text_input('Username')
+    password = st.text_input('Password', type='password')
     if st.button('Login'):
         user = db.fetch_user(username)
         if user:
             if user['password'] == password:
-                st.success('Login successful')
+                st.success(f'Welcome to Stockly ' + user['name'])
                 return user
             else:
                 st.error('Incorrect password')
@@ -44,32 +46,36 @@ st.sidebar.header('STOCKLY APP')
 opts = ['Login', 'Sign Up']
 option = st.sidebar.selectbox("Login to continue:", opts)
 if option == 'Login':
-        user = login()
-        st.sidebar.header('Navigation')
+        login()
+        if st.session_state.onclick:
+            st.sidebar.header('Navigation')
 
-        page_options = ['Home', 'Predictions', 'Company Charts']
-        page = st.sidebar.radio("Go to:", (
-                                        'Home',
-                                        'Predictions',
-                                        'Company Charts'
-                                     ))
+            page_options = ['Home', 'Predictions', 'Company Charts']
+            page = st.sidebar.radio("Go to:", (
+                'Home',
+                'Predictions',
+                'Company Charts'
+            ))
 
-        if page == 'Home':
-            from home import run
-            run()
+            if page == 'Home':
+                from home import run
 
-        elif page == 'Predictions':
-            from predictions import run
-            run()
+                run()
 
-        else:
-            from company_charts import run
-            run()
+            elif page == 'Predictions':
+                from predictions import run
+
+                run()
+
+            else:
+                from company_charts import run
+
+                run()
+
 
 else:
     st.sidebar.warning("Please log in after sign up to view the content.")
     signup()
-
 
 
 
